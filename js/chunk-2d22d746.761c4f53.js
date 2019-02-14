813150106c038c8aba9959f73393d2286473b185
Alexander-Polyakov <Alexander-Polyakov@users.noreply.github.com>
2019-02-14 11:44:45 +0300

    Add files via upload

---
+++ js/chunk-2d22d746.761c4f53.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(e,n,t){"use strict";t.r(n);var u=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},c=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h1",[e._v("This is an About page")])])}],r=t("2877"),s={},a=Object(r["a"])(s,u,c,!1,null,null,null);n["default"]=a.exports}}]);
+//# sourceMappingURL=chunk-2d22d746.761c4f53.js.map
